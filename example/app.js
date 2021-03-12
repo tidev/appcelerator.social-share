0d32826f05d4c504df35a468e865c6534ca430e2
Vikas Goyal <38314488+vikas-goyal-syncplicity@users.noreply.github.com>
2021-03-12 14:29:01 +0530

    feat(sample): added share sample

--- example/app.js
+++ example/app.js
@@ -1,22 +1,80 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
+/* eslint-disable no-alert */
 
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor: 'white'
+var socialshare = require('appcelerator.socialshare');
+var mainWindow = Ti.UI.createWindow({
+	backgroundColor: 'White',
+	titleAttributes: { color: 'blue' }
 });
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
+var navCentralWindow = Ti.UI.createNavigationWindow({
+	window: mainWindow
+});
+var itemProvider = Ti.UI.createButton({
+	font: { fontSize: 20 },
+	title: 'Share Using Activity Item Provider',
+	top: 80
+});
 
-// TODO: write your module tests here
-var social_share = require('appcelerator.socialshare');
-Ti.API.info('module is => ' + social_share);
+itemProvider.addEventListener('click', function () {
+	const itemProvider = socialshare.createActivityItemProvider({
+		contentType: socialshare.Content_Type_String,
+		placeHolder: 'placeholder',
+		activityType: socialshare.Activity_Type_Post_To_Facebook
+	});
+	global.itemProvider = itemProvider;
+	itemProvider.fetchItem = function () {
+		return 'This is Text To Share';
+	};
+	itemProvider.activityType = socialshare.Activity_Type_Message;
+	const customActivity = socialshare.createCustomActivity({
+		category: socialshare.Activity_Category_Share,
+		title: 'url share',
+	});
+	global.customActivity = customActivity;
+	socialshare.shareWithItems({
+		activityItems: [ itemProvider ],
+		activities: [ customActivity ],
+		completionWithItemsHandler: function (e) {
+			if (e.errorCode !== null || e.errorDomain !== null || e.errorDescription !== null) {
+				alert('Error Occured: ' + e.errorDescription);
+				return;
+			}
+		}
+	});
+});
+mainWindow.add(itemProvider);
 
-label.text = social_share.example();
+var itemSource = Ti.UI.createButton({
+	font: { fontSize: 20 },
+	title: 'Share using Item Source',
+	top: 140,
+});
+itemSource.addEventListener('click', function () {
+	const itemSource = socialshare.createActivityItemSource({
+		contentType: socialshare.Content_Type_String
+	});
+	global.itemSource = itemSource;
+	itemSource.fetchItem = function () {
+		return 'This is Text To Share';
+	};
+	itemSource.fetchPlaceholderItem = function () {
+		return 'This is Text To Share';
+	};
+	const customActivity = socialshare.createCustomActivity({
+		category: socialshare.Activity_Category_Share,
+		title: 'url share',
+	});
+	global.customActivity1 = customActivity;
+	socialshare.shareWithItems({
+		activityItems: [ itemSource ],
+		activities: [ customActivity ],
+		completionWithItemsHandler: function (e) {
+			if (e.errorCode !== null || e.errorDomain !== null || e.errorDescription !== null) {
+				alert('Error Occured: ' + e.errorDescription);
+				return;
+			}
+		}
+	});
+});
+mainWindow.add(itemSource);
 
-Ti.API.info('module exampleProp is => ' + social_share.exampleProp);
-social_share.exampleProp = 'This is a test value';
-
+navCentralWindow.open();
